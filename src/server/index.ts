ec4fe37225f72a3e1ffd3719e275219cb1cc2498
Victor Ribeiro <ribeir145@gmail.com>
2025-05-12 22:47:29 -0300

    Removed example route

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,7 @@
 import { router } from './trpc';
-import { publicProcedure } from './trpc';
 import { taskRouter } from './routers/task';
 
 export const appRouter = router({
-    getExampleList: publicProcedure.query(async () => {
-        return [1, 2, 3, 4];
-    }),
     task: taskRouter
 });
 
